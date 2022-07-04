f1b98fae64f734e545ae67cb069e4480160b25e2
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-07-04 14:35:53 +0530

    Added content to landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -25,15 +25,29 @@ export function LandingPage() {
             </div>
             <div id="About" className="cover">
                 <h1>About</h1>
-                <p>B Airways is blah blah ...</p>
+                <p className="text-center"><strong>B Airways</strong> is a subsidiary of <strong>Virgin
+                    Airlines </strong> but functions independently.<br/> It currently caters only
+                    to the needs of small distance, internal flights in Indonesia. <br/> B Airways founded on 2000 and
+                    currently a fastest growing airlines in the world </p>
             </div>
             <div id="Services" className="cover">
                 <h1>Services</h1>
-                <p>We provide blahhh</p>
+                <p>We provide airline transportation for distinct locations in the world and provide internal operations
+                    in Indoneasia. </p>
             </div>
             <div id="Contact" className="cover">
                 <h1>Contact</h1>
-                <p>Contact us on</p>
+                <p>Find us on</p>
+                <div className="d-flex justify-content-evenly" style={{width: "60%"}}>
+                    <img
+                        src={"https://img.shields.io/badge/-Facebook-blue?style=for-the-badge&labelColor=blue&logo=facebook&logoColor=white"}/>
+                    <img
+                        src={"https://img.shields.io/badge/-Twitter-9cf?style=for-the-badge&labelColor=9cf&logo=twitter&logoColor=white"}/>
+                    <img
+                        src={"https://img.shields.io/badge/-Instagram-ff69b4?style=for-the-badge&labelColor=ff69b4&logo=instagram&logoColor=white"}/>
+                    <img
+                        src={"https://img.shields.io/badge/-LinkedIn-blue?style=for-the-badge&labelColor=blue&logo=linkedin&logoColor=white"}/>
+                </div>
             </div>
         </div>
     );
